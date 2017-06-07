bffe391fde99ad5562607848e669530c0fad7612
Edmon <comlaterra@gmail.com>
2017-06-07 16:47:55 +0200

    Add script to do the iOS images automagically

---
+++ Scripts/iphone_white.jsx
@@ -0,0 +1,7 @@
+
+#target photoshop
+#include 'deviceArtGenerator.jsx';
+
+app.bringToFront();
+
+createDeviceArt('iPhone5_white', activeDocument.fullName, true, true, true, true, true); //iPhone5_white device with transparent background.
