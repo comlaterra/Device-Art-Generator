637350505b09c7f0dd371dc5628b70b85d603961
Edmon <comlaterra@gmail.com>
2017-07-23 09:07:15 +0200

    Add ipad portrait ready-to-use

---
+++ Scripts/ipad_white.jsx
@@ -0,0 +1,7 @@
+
+#target photoshop
+#include 'deviceArtGenerator.jsx';
+
+app.bringToFront();
+
+createDeviceArt('iPad3_white', activeDocument.fullName, true, true, true, true, true); //iPhone5_white device with transparent background.
